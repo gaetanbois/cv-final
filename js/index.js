b6f57f918fc3919c0ed80344ca041332e2ceed2c
gaetanbois <g.bois17@gmail.com>
2017-10-04 15:41:09 +0200

    reste lien vers github

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+$('a[href*="#"]').not('[href="#"]').click(function(b) {
+  if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+    var a = $(this.hash), a = a.length ? a : $("[name=" + this.hash.slice(1) + "]");
+    a.length && (b.preventDefault(), $("html, body").animate({scrollTop:a.offset().top}, 500, function() {
+    }));
+  }
+});
+
 $(function() {
 	$('a[href*=#]').on('click', function(e) {
 		e.preventDefault();
@@ -8,3 +16,4 @@ $(function() {
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
 });
+
